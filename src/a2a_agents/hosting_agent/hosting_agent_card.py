080d78967f275eaa7c67b0d0536e08a2ed95c2d3
Dave Wang <wangdave@google.com>
2026-02-23 02:16:55 +0000

    chore: update packages for GE

--- src/a2a_agents/hosting_agent/hosting_agent_card.py
+++ src/a2a_agents/hosting_agent/hosting_agent_card.py
@@ -41,7 +41,7 @@ hosting_agent_skill = AgentSkill(
 
 # Use the helper function to create a complete Agent Card
 hosting_agent_card = create_agent_card(
-    agent_name="Hosting Agent adk-mb - ADK",
+    agent_name="Hosting Agent ADK-MB",
     description="A helpful assistant agent that can answer questions.",
     skills=[hosting_agent_skill],
 )
