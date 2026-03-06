36c497a5fcde3f7aea04b2e341ba1254a61dc904
Dave Wang <wangdave@google.com>
2026-03-06 02:30:18 +0000

    fix(terraform): ignore deployment_spec and package_spec changes in agent_engine.tf to prevent reverting SDK deployments

--- deployment/terraform/agent_engine.tf
+++ deployment/terraform/agent_engine.tf
@@ -71,7 +71,9 @@ resource "google_vertex_ai_reasoning_engine" "agent" {
   lifecycle {
     ignore_changes = [
       spec[0].source_code_spec,
-      spec[0].deployment_spec[0].env,
+      spec[0].package_spec,
+      spec[0].deployment_spec,
+      display_name
     ]
   }
 }
